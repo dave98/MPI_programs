95dfe825838329d5fd3c3eb36f59c5ec4ef5601a
dave98 <jose.mamani.vilca@ucsp.edu.pe>
2018-09-17 00:32:23 -0500

    3

--- ping_pong/main.sh
+++ ping_pong/main.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-#PBS -N test_main
 #PBS -l nodes=1:ppn=1
+#PBS -N test_main
+#PBS -m bea
 
+#start job from the directory it was submitted
 cd  $PBS_O_WORKDIR
 
-/opt/shared/openmpi-2.0.1/bin/mpicxx CMD BATCH main.cpp
+/opt/shared/openmpi-2.0.1/bin/mpicxx $MPI_HOSTS app
